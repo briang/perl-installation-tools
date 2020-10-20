18656544bd0c08ea4bdec0e40f15e9008af0cb0f
brian greenfield <briang@cpan.org>
2020-10-20 15:58:44 +0100

    move code inside sub main

--- perlarch.pl
+++ perlarch.pl
@@ -14,26 +14,31 @@ my $PERLS                  = "/home/cpan/perlbrew/perls";
 
 my $perl = 'perl-5.32.0';
 
-for my $p ('', '-Dusequadmath', '--ld') {
-    for my $q ('', '--threaded') {
-        my $name = my $options = join ' ', grep { length } $p, $q, '--noman';
-        for ($name) {
-            s/-Dusequadmath/qm/;
-            s/--ld/ld/;
-            s/--threaded/th/;
-            s/--noman//;
-            s/ /-/g;
-            s/-$//;
+main();
+exit;
+
+sub main {
+    for my $p ('', '-Dusequadmath', '--ld') {
+        for my $q ('', '--threaded') {
+            my $name = my $options = join ' ', grep { length } $p, $q, '--noman';
+            for ($name) {
+                s/-Dusequadmath/qm/;
+                s/--ld/ld/;
+                s/--threaded/th/;
+                s/--noman//;
+                s/ /-/g;
+                s/-$//;
+            }
+
+            $name = join '-', $perl, ($name || ());
+            next if -d "$PERLS/$name";
+
+            my $perl_tar_gz = "$perl.tar.gz";
+            my @perlbrew = split ' ', join ' ',
+              "perlbrew install $OTHER_PERLBREW_OPTIONS",
+              "$PERL_SOURCE/$perl_tar_gz --as $name $options";
+            say "-->@perlbrew";
+            system(@perlbrew) == 0 or die $!;
         }
-
-        $name = join '-', $perl, ($name || ());
-        next if -d "$PERLS/$name";
-
-        my $perl_tar_gz = "$perl.tar.gz";
-        my @perlbrew = split ' ', join ' ',
-          "perlbrew install $OTHER_PERLBREW_OPTIONS",
-          "$PERL_SOURCE/$perl_tar_gz --as $name $options";
-        say "-->@perlbrew";
-        system(@perlbrew) == 0 or die $!;
     }
 }
