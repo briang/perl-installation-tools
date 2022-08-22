28733738d3eda43cd6ba61d0ae3547f88f94b163
brian greenfield <briang@cpan.org>
2022-08-22 17:12:40 +0100

    replace `cleanup` with `cleanup-perlbrew-builds.sh`

---
+++ cleanup-perlbrew-builds.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Template from http://kvz.io/blog/2013/11/21/bash-best-practices/
+
+set -o errexit  # exit on error
+set -o pipefail # trap pipe fails
+set -o nounset  # trap unset vars
+# set -o xtrace
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+# __root="$(cd "$(dirname "${__dir}")" && pwd)"
+# ^^^ change this as it depends on the script ^^^
+
+arg1="${1:-}"
+################################################################################
+rm -rf ~/perlbrew/build/*
+rm     ~/perlbrew/build.*.log
